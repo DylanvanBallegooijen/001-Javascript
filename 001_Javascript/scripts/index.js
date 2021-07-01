3f6428fede58d4ae6d79e0e6b8598e8230704780
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-07-01 16:03:43 +0200

    Add files via upload

---
+++ 001_Javascript/scripts/index.js
@@ -0,0 +1,44 @@
+     
+        let name ='Dylan';
+        console.log(name);
+        // een variable met de naam "if" kan niet omdat deze naam al gereserveerd is
+        
+        // addition
+        let x = 4;
+        let y = 4;
+        let z = x + y; 
+        console.log (z)
+
+        let a = "10";
+        let b = "20";
+        let c = a + b;
+        console.log (c)
+        // numbers kan je bijelkaar optellen zie bovenstaande
+        // strings kan je bijelkaar optellen zie bovenstaande
+
+        //subtraction
+        let d = 10;
+        let e = 4;
+        let f = d - e; 
+        console.log (f)
+
+        //multiplication
+        let g = 10;
+        let h = 4;
+        let i = g * h; 
+        console.log (i)
+
+        //division
+        let j = 10;
+        let k = 4;
+        let l = j / k;
+        console.log (l)
+
+        //modulus
+        let m = 10;
+        let n = 4;
+        let o = m % n;
+        console.log (o)
+
+        let age = "vierendertig";
+        console.log (typeof 34)
